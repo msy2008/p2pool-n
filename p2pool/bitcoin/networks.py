ca685348c1ccbe3ec13d03ff8bce95162c453675
msy2008 <963858700@qq.com>
2022-09-23 20:46:29 +0800

    Add support for Infinitecoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -85,6 +85,28 @@ def check_genesis_block(bitcoind, genesis_block_hash):
         defer.returnValue(True)
 
 nets = dict(
+    infinitecoin=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=9321,
+        ADDRESS_VERSION=102,
+        RPC_PORT=9322,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'infinitecoin' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 524288*1000000000 >> (height + 1)//86400,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=30, # s
+        SYMBOL='IFC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Infinitecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Infinitecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.Infinitecoin'), 'infinitecoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='https://chainz.cryptoid.info/ifc/block.dws?',
+        ADDRESS_EXPLORER_URL_PREFIX='https://chainz.cryptoid.info/ifc/address.dws?',
+		TX_EXPLORER_URL_PREFIX='https://chainz.cryptoid.info/ifc/tx.dws?',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD = 1e8,
+    ),
+
     bitcoin=math.Object(
         P2P_PREFIX='f9beb4d9'.decode('hex'),
         P2P_PORT=8333,
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -8,6 +8,25 @@ from p2pool.util import math
 # changes can be done by changing one, then the other
 
 nets = dict(
+    infinitecoin=math.Object(
+        PARENT=networks.nets['infinitecoin'],
+        SHARE_PERIOD=15, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=12, # blocks
+        IDENTIFIER = '1137d5ffc69234ff'.decode('hex'),
+        PREFIX = '1237d5ffc69234ff'.decode('hex'),
+        P2P_PORT=9392,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9391,
+        BOOTSTRAP_ADDRS=['8.209.112.218','220.179.77.92'],
+        ANNOUNCE_CHANNEL='#p2pool-ifc',
+        VERSION_CHECK=lambda v: True,
+    ),
+
     bitcoin=math.Object(
         PARENT=networks.nets['bitcoin'],
         SHARE_PERIOD=30, # seconds
